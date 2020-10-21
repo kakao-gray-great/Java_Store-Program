4c3838ebdc4e56db2bb603fc3035d75981d34253
oopsys6 <operasystem6@naver.com>
2020-10-21 20:56:27 +0900

    ADD : Item Class

--- mgr/Item.java
+++ mgr/Item.java
@@ -3,24 +3,28 @@ package mgr;
 import java.util.Scanner;
 
 public class Item implements Manageable {
-    private String itemName;
     private String code;
+    private String itemName;
     private int price;
 
     @Override
     public void read(Scanner scan) {
-        itemName = scan.next();
         code = scan.next();
+        itemName = scan.next();
         price = scan.nextInt();
     }
 
     @Override
     public void print() {
-
+        System.out.printf("[%s] %s (%d)", code, itemName, price);
     }
 
     @Override
     public boolean matches(String kwd) {
+        if (code.contentEquals(kwd))
+            return true;
+        if (itemName.contentEquals(kwd))
+            return true;
         return false;
     }
 }
